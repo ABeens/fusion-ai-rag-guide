b302606e559daa1fe6d03df41a308da451262312
Abraham <41970794+ABeens@users.noreply.github.com>
2025-11-23 19:42:02 -0600

    Ajuste de links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/fusion-ai-rag-guide',
+    site: 'https://abeens.github.io'
+});
